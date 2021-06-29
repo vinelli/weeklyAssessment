16acb3d203c819bc99fcdc59b0125d0820056ce8
vinelli <85247062+vinelli@users.noreply.github.com>
2021-06-29 22:00:28 +0100

    week four assessment

---
+++ main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
+
+
+fun evenlySpaced() {
+    val a: Int = 2
+    val b: Int = 4
+    val c: Int = 6
+    val d: Int = 3
+    if (c-b == b-a ) {
+        println("True")
+        else if (c - b == c - d) {
+            println("False")
+        }
+    }
