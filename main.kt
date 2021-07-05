76d4658178dac1bebd92e146f05dacfda744b9d0
vinelli <85247062+vinelli@users.noreply.github.com>
2021-07-05 21:05:47 +0100

    week five assessment

--- main.kt
+++ main.kt
@@ -1,16 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-}
-
-
-fun evenlySpaced() {
-    val a: Int = 2
-    val b: Int = 4
-    val c: Int = 6
-    val d: Int = 3
-    if (c-b == b-a ) {
-        println("True")
-        else if (c - b == c - d) {
-            println("False")
+fun removeVowels(s: String): String {
+    val re = StringBuilder()
+    for (c in s) {
+        if (c != 'A' && c != 'a'
+            && c != 'E' && c != 'e'
+            && c != 'I' && c != 'i'
+            && c != 'O' && c != 'o'
+            && c != 'U' && c != 'u') {
+            re.append(c)
         }
     }
+    return re.toString()
+}
+
+fun main() {
+    println(removeVowels("My Name is Kanayo"))
+}
